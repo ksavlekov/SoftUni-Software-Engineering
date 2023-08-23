5debafc6c7eb1caa9a7b4afd3080d80d5e52e660
ksavlekov <ksavlekov@gmail.com>
2023-08-24 02:19:29 +0300

    new file

---
+++ JavaBasics/firstStepsInCoding/ex05_TrapeziodArea.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class ex05_TrapeziodArea {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double b1 = Double.parseDouble(scanner.nextLine());
+        double b2 = Double.parseDouble(scanner.nextLine());
+        double h = Double.parseDouble(scanner.nextLine());
+
+        double trapeziodArea = (b1 + b2) * h / 2;
+
+        System.out.println(trapeziodArea);
+    }
+}
