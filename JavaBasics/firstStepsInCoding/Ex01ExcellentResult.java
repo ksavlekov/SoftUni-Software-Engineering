e29a54f89a10c0ed5e2a1ef04ba6182bd2b63b34
ksavlekov <ksavlekov@gmail.com>
2023-08-31 21:50:52 +0300

    new file

---
+++ JavaBasics/firstStepsInCoding/Ex01ExcellentResult.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Ex01ExcellentResult {
+    public static void main(String [] args) {
+       Scanner sc = new Scanner(System.in);
+
+       double estimate = Double.parseDouble(sc.nextLine());
+
+       if (estimate >= 5.50) {
+           System.out.println("Excellent!");
+       } else {
+
+       }
+    }
+}
