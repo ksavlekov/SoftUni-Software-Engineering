e240bfd76d26d6a9513f1cb9ae4f0343f97a1894
ksavlekov <ksavlekov@gmail.com>
2023-09-24 21:09:21 +0300

    new file

---
+++ JavaBasics/firstStepsInCoding/Expression2.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Expression2 {
+    public static void main(String[] args) {
+
+        System.out.println((3522 + 52353) * 23 - (2336 * 501 + 23432 - 6743) * 3);
+
+    }
+}
