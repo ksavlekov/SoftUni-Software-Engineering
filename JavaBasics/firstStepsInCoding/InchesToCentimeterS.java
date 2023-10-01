b76df4a38693e65a948d27b8a349b59651edd212
ksavlekov <ksavlekov@gmail.com>
2023-10-01 08:34:04 +0300

    new file

---
+++ JavaBasics/firstStepsInCoding/InchesToCentimeterS.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class InchesToCentimeterS {
+
+    public static void main(String[] args){
+
+       Scanner scanner = new Scanner(System.in);
+
+       double input = Double.parseDouble(scanner.nextLine());
+
+       System.out.println(input * 2.54);
+
+    }
+}
