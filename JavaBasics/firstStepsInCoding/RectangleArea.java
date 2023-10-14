b27cece7f592b51a27c04a01a5d6d18debc46563
ksavlekov <ksavlekov@gmail.com>
2023-10-14 04:04:14 +0300

    new file

---
+++ JavaBasics/firstStepsInCoding/RectangleArea.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class RectangleArea {
+
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        int a = Integer.parseInt(scanner.nextLine());
+        int b = Integer.parseInt(scanner.nextLine());
+
+        int sum = a + b;
+
+        System.out.println(sum);
+    }
+}
