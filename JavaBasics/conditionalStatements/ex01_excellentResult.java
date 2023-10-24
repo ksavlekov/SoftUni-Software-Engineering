be4ee6e2beb8831044a2832906d5223e523ab631
ksavlekov <ksavlekov@gmail.com>
2023-10-24 20:16:48 +0300

    add new file

---
+++ JavaBasics/conditionalStatements/ex01_excellentResult.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class ex01_excellentResult {
+
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        double result = Double.parseDouble(scanner.nextLine());
+
+        if (result >= 5) {
+            System.out.println("Excellent!");
+        }else {
+
+        }
+
+    }
+}
