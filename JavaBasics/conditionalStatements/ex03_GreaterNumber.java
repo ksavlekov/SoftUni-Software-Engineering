0891954e08719703c5e16a7777526e38def5ccfe
ksavlekov <ksavlekov@gmail.com>
2023-10-30 09:37:12 +0200

    add new file

---
+++ JavaBasics/conditionalStatements/ex03_GreaterNumber.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class ex03_GreaterNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = Integer.parseInt(scanner.nextLine());
+        int b = Integer.parseInt(scanner.nextLine());
+
+        if (a > b) {
+            System.out.println("Greater number: " + a);
+
+        } else {
+            System.out.println("Greater number: " + b);
+        }
+    }
+}
