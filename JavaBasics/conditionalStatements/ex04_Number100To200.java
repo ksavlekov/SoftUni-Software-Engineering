26f792854b347d3524e5f2142cb3f94d35ffa3d2
ksavlekov <ksavlekov@gmail.com>
2023-10-31 15:43:27 +0200

    add new file

---
+++ JavaBasics/conditionalStatements/ex04_Number100To200.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class ex04_Number100To200 {
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        int a = Integer.parseInt(scanner.nextLine());
+
+        if (a < 100){
+            System.out.println("Less than 100");
+        }else if (a >= 100 && a <= 200){
+            System.out.println("Between 100 and 200");
+        }else{
+            System.out.println("Greater than 200");
+        }
+
+    }
+}
