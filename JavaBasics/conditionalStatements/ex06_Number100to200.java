cce3eb3f9b143485e79e5b03cda6b64bacb85aa4
ksavlekov <ksavlekov@gmail.com>
2023-11-14 06:49:20 +0200

    add new file

---
+++ JavaBasics/conditionalStatements/ex06_Number100to200.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class ex06_Number100to200 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int number = Integer.parseInt(scanner.nextLine());
+
+
+    }
+}
