63cf853794c141c26ff561940f499c31fdd0e68b
ksavlekov <ksavlekov@gmail.com>
2024-03-09 02:46:05 +0200

    add new file

---
+++ JavaBasics/ForLoop/Ex02_LatinLetters.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Ex02_LatinLetters {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        for (char i = 'a'; i <= 'z'; i++) {
+            System.out.println(i);
+        }
+    }
+}
