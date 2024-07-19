1ea839d09a6d52e81b5c0644d0d6504f2b849e6a
ksavlekov <ksavlekov@gmail.com>
2024-07-19 05:45:13 +0300

    add new file

---
+++ JavaBasics/nestedLoops/Ex05_EqualSumsEvenOddPosition.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Ex05_EqualSumsEvenOddPosition {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
