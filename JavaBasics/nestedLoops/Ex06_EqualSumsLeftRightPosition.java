4f5b327d310352d1fb856991a7ea11b2c7f940fa
ksavlekov <ksavlekov@gmail.com>
2024-07-25 03:38:28 +0300

    add new file

---
+++ JavaBasics/nestedLoops/Ex06_EqualSumsLeftRightPosition.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Ex06_EqualSumsLeftRightPosition {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int firstNumber = Integer.parseInt(scanner.nextLine());
+        int secondNumber = Integer.parseInt(scanner.nextLine());
+
+        int sum = 0;
+
+        for (int i = 10000; i <= 99999; i++)
+
+            if (firstNumber < secondNumber) {
+
+
+
+        }
+    }
+}
