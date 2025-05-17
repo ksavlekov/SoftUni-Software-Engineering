06c687399f1cf8cf38ac27693740e26a4e03dd13
ksavlekov <ksavlekov@gmail.com>
2025-05-17 06:36:12 +0300

    add new file

---
+++ JavaFundamentals/associativeArrays/Ex02_A_MinerTask.java
@@ -0,0 +1,38 @@
+package AssociativeArrays;
+
+import java.util.LinkedHashMap;
+import java.util.Scanner;
+
+public class Ex02_A_MinerTask {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        LinkedHashMap<String, Integer> resources = new LinkedHashMap<>();
+
+        String input = "";
+
+        while(!"stop".equals(input = scanner.nextLine())) {
+
+            String resource = input;
+
+            int quantity = Integer.parseInt(scanner.nextLine());
+
+            if (!resources.containsKey(resource)) {
+
+                resources.put(resource, quantity);
+
+            } else {
+
+                resources.put(resource, resources.get(resource) + quantity);
+
+            }
+
+
+        }
+
+        resources.forEach((key, value) ->
+                System.out.println(String.format("%s -> %d", key, value)));
+
+
+    }
+}
