6f8a397fce3fc791d845745dd1624150dc2524f6
ksavlekov <ksavlekov@gmail.com>
2025-05-27 19:23:22 +0300

    add new file

---
+++ JavaFundamentals/introAndBasicSyntax/Ex02Passed.java
@@ -0,0 +1,17 @@
+package IntroAndBasicSyntax;
+
+import java.util.Scanner;
+
+public class Ex02Passed {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double grade = Double.parseDouble(sc.nextLine());
+
+        if (grade >= 3) {
+            System.out.println("Passed!");
+        } else {
+
+        }
+    }
+}
