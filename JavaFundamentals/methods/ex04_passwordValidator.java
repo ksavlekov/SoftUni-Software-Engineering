8e7e938de3dcc7d5d19d6e3f3fda108555beef44
ksavlekov <ksavlekov@gmail.com>
2025-07-18 05:03:21 +0300

    add new file

---
+++ JavaFundamentals/methods/ex04_passwordValidator.java
@@ -0,0 +1,17 @@
+package Methods;
+
+import java.util.Scanner;
+
+public class ex04_passwordValidator {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+
+    static {
+
+
+    }
+}
