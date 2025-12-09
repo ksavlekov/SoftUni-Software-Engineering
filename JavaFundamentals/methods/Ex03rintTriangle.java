049d1960b82e9a893c5bf32c343ba57247da9284
ksavlekov <ksavlekov@gmail.com>
2025-12-09 02:15:16 +0200

    add new file

---
+++ JavaFundamentals/methods/Ex03rintTriangle.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Ex03rintTriangle {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 1; i <= n; i++) {
+            printLine(i);
+        }
+        for (int i = n - 1; i > 0; i--) {
+            printLine(i);
+        }
+
+    }
+
+    public static void printLine(int i) {
+        for (int j = 1; j <= i; j++) {
+            System.out.print(j + " ");
+        }
+        System.out.println();
+    }
+}
